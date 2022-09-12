609e7c47e6065fa2cb3942b8ad1619f0aedceed3
Clay Coleman <claycoleman94@gamil.com>
2022-09-12 12:01:18 -0400

    inital upload

---
+++ APiExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+
+for (int i = 0; i < 5; i++)
+{
+
+ var client1 = new HttpClient();
+
+ var ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+ var ronResponses = client1.GetStringAsync(ronSwansonURL).Result;
+
+ var ronQuote = JArray.Parse(ronResponses);
+
+
+    Console.WriteLine("Ron:");
+ Console.WriteLine($"{ronQuote[0]}");
+
+
+
+ var client2 = new HttpClient();
+
+ var kanyeURL = "https://api.kanye.rest";
+
+ var kanyeResponses = client2.GetStringAsync(kanyeURL).Result;
+
+ var kanyeQuote = JObject.Parse(kanyeResponses).GetValue("quote").ToString();
+    Console.WriteLine("Kanye:");
+ Console.WriteLine($"{kanyeQuote}");
+    Console.WriteLine();
+
+}
